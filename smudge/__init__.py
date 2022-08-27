575de2aab1e455eef8ec13e5a42b3d6234d93aa5
David Quartarolo <david.quartarolo@sectigo.com>
2022-08-27 14:33:45 -0400

    fix: Corrected pylint issues associated with the init, version, and test_signature_matching files.

--- smudge/__init__.py
+++ smudge/__init__.py
@@ -1,17 +1,18 @@
+""" SMUDGE """
 #   _____ __  __ _    _ _____   _____ ______
 #  / ____|  \/  | |  | |  __ \ / ____|  ____|
 # | (___ | \  / | |  | | |  | | |  __| |__
 #  \___ \| |\/| | |  | | |  | | | |_ |  __|
 #  ____) | |  | | |__| | |__| | |__| | |____
 # |_____/|_|  |_|\____/|_____/ \_____|______|
-#
+
 
 from os.path import exists
 import sqlite3
 
-from .utils import passive_data
-from .utils import pull_data
-from .utils import tcp_sig
+from .utils import PassiveData
+from .utils import PullData
+from .utils import TcpSig
 
 from .utils import Quirk
-from .utils import signature
+from .utils import Signature
