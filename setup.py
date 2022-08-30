6913c852bb7dd5564061cd0b304dcc9f4d57504f
David Quartarolo <david.quartarolo@sectigo.com>
2022-08-30 11:16:31 -0400

    fix: Pylint issue with setup.py.

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
+"""
+SMUDGE setup.py
+"""
 from setuptools import setup
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
