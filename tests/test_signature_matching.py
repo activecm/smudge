40a58f21fe36953edbe5b6355b90cd3335a86dc0
David Quartarolo <david.quartarolo@gmail.com>
2022-12-20 17:09:06 -0500

    feat(testing): Add pcap for testing.

--- tests/test_signature_matching.py
+++ tests/test_signature_matching.py
@@ -3,21 +3,22 @@ import pytest
 from scapy.all import sniff
 from smudge import Signature
 
+pcap_file = "test.pcap"
 
 def test_signature_1():
     '''Signature.'''
-    packet_1 = sniff(offline="passer.pcap")[0]
+    packet_1 = sniff(offline=pcap_file)[0]
     signature_1 = Signature(packet_1)
     assert signature_1.version == str(4)
 
 def test_process_options():
     '''Pytest for Process Options static method.'''
-    packet_1 = sniff(offline="passer.pcap")[0]
+    packet_1 = sniff(offline=pcap_file)[0]
     signature_1 = Signature.process_options(packet_1)
     assert signature_1 == '?n'
 
 def test_version():
     '''Pytest for Version property.'''
-    packet_1 = sniff(offline="passer.pcap")[0]
+    packet_1 = sniff(offline=pcap_file)[0]
     signature_1 = Signature(packet_1).version
     assert signature_1 == '4'
