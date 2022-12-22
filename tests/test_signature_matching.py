5255e284b0c7fee80708edc0f75f1a9e82855661
David Quartarolo <david.quartarolo@gmail.com>
2022-12-21 21:33:05 -0500

    fix(style): Pylint C0304, C0103

--- tests/test_signature_matching.py
+++ tests/test_signature_matching.py
@@ -2,22 +2,22 @@
 from scapy.all import sniff
 from smudge import Signature
 
-pcap_file = "test.pcap"
+PCAP_FILE = "test.pcap"
 
 def test_signature_1():
     '''Signature.'''
-    packet_1 = sniff(offline=pcap_file)[0]
+    packet_1 = sniff(offline=PCAP_FILE)[0]
     signature_1 = Signature(packet_1)
     assert signature_1.version == str(4)
 
 def test_process_options():
     '''Pytest for Process Options static method.'''
-    packet_1 = sniff(offline=pcap_file)[0]
+    packet_1 = sniff(offline=PCAP_FILE)[0]
     signature_1 = Signature.process_options(packet_1)
     assert signature_1 == '?n'
 
 def test_version():
     '''Pytest for Version property.'''
-    packet_1 = sniff(offline=pcap_file)[0]
+    packet_1 = sniff(offline=PCAP_FILE)[0]
     signature_1 = Signature(packet_1).version
     assert signature_1 == '4'
